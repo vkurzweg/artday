42d159a59b3349447d22a35bde14cc0e01c3df7a
Victoria Dunham <vkurzweg@gmail.com>
2016-09-16 12:03:16 -0700

    initial

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Signed up! Welcome to Artday."
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
